c93352101933109cfb30b5df4ae7c3e2e72edf15
Olga Shilenkova <Olga1shilenkova@gmail.com>
2021-11-24 17:31:12 -0600

    First commit of letter position in the sentence function

---
+++ letterPositions.js
@@ -0,0 +1,50 @@
+/*
+implement a new function letterPositions which will return all the indices (zero-based positions) in the string where each character is found.
+
+
+*/
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅ Assertion Passed `);
+  } else {
+    console.log(`🛑 Assertion Failed `);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+///////FUNCTION ITSELF
+
+const letterPositions = function(sentence) {
+  const results = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+
+    if (!results[sentence[i]]) {
+      //here we create an array
+      results[sentence[i]] = [i];
+    } else {
+      //here we push index to the array
+      //we can use push to propery/key of the object
+      results[sentence[i]].push(i);
+    }
+  }
+  return results;
+};
+
+
+console.log(letterPositions("hello"));
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").o, [4]);

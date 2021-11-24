95fe33ad7f4084d3f1c5191d7abf24fee81ee9f6
Olga Shilenkova <Olga1shilenkova@gmail.com>
2021-11-24 16:34:55 -0600

    First commit of count letters from sentence

---
+++ countLetters.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ Assertion Passed: ${[actual]} === ${[expected]}`);
+  } else {
+    console.log(`🛑 🛑Assertion Failed: ${[actual]} !== ${[expected]}`);
+
+  }
+
+};
+
+/*
+The function should take in a sentence (as a string) and then return a count of each of the letters in that sentence.
+*/
+const countLetters = function(sentence){
+
+  const results = {};
+  for (const letter of sentence){
+
+    if(results[letter]){
+      results[letter] += 1;
+    }else{
+      results[letter] = 1;
+    }
+  }
+return results;
+
+};
+
+// console.log( countLetters("lighthouse in the house"));
+console.log(countLetters("hello"));
+
+//get access to value of key "h"
+console.log(countLetters("h")["h"]);
+// how to implement assertEqual for this function???
+assertEqual(countLetters("h")["h"], 1); //PASS
+
+console.log(countLetters("hello")["l"]);
+assertEqual(countLetters("hello")["l"], 2);//PASS

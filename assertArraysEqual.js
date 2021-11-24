211cb65910fa0119a683c744ddff97737ac36eb4
Olga Shilenkova <Olga1shilenkova@gmail.com>
2021-11-23 21:24:35 -0600

    assertion of arrays comparison type pass or fail in console

---
+++ assertArraysEqual.js
@@ -0,0 +1,36 @@
+/*
+Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
+*/
+
+const assertArraysEqual = function(eqArrays) {
+  if (eqArrays === true) {
+    console.log(`✅ Assertion Passed `);
+  } else {
+    console.log(`🛑 Assertion Failed `);
+  }
+};
+
+const eqArrays = function (arr1, arr2) {
+
+  //check if arrays length match
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  //iterate through arr1 to get the values
+  for (let i = 0; i < arr1.length; i++) {
+    // console.log(arr1[i]);
+    //if values of arr1 and arr2 is does not complitly match return false
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3])); // => should PASS
+assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1])); // => false => should FAIL
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => should PASS
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false => should FAIL
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2"])) // => false => FAIL
+assertArraysEqual(eqArrays(["a", "b", "c"], ["a", "b", "c"])) // => true => PASS

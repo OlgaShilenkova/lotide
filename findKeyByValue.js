7f51a3da80b98c5d3bece1a8199deb0062dec8b3
Olga Shilenkova <Olga1shilenkova@gmail.com>
2021-11-24 19:20:07 -0600

    First commit of finding key by givin value function

---
+++ findKeyByValue.js
@@ -0,0 +1,48 @@
+/*
+Implement the function findKeyByValue which takes in an object and a value. It should scan the object and return the first key which contains the given value. If no key with that given value is found, then it should return undefined.
+*/
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ Assertion Passed: ${[actual]} === ${[expected]}`);
+  } else {
+    console.log(`🛑 🛑Assertion Failed: ${[actual]} !== ${[expected]}`);
+  }
+};
+
+//FUNCTION ITSELF
+
+const findKeyByValue = function(object, valueToFind) {
+
+  const keysOfObject = Object.keys(object);
+
+  for (const key of keysOfObject) {
+    // get access to values
+
+    let value = object[key];
+
+    // compare value that we have with given valueToFind
+    if (value === valueToFind) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+
+const bestTVShowsByGenre = {
+  "sci_fi": "The Expanse",
+  "comedy": "Brooklyn Nine-Nine",
+  "drama": "The Wire"
+};
+
+// console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+// console.log(findKeyByValue(bestTVShowsByGenre, "The "));
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "musical");

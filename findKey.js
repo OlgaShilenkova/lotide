f45dd8e7ffc5b4d81b2f6e1d21610e70e8108406
Olga Shilenkova <Olga1shilenkova@gmail.com>
2021-11-25 20:58:11 -0600

    find key by value in callback

---
+++ findKey.js
@@ -0,0 +1,73 @@
+/*
+Implement the function findKey which takes
+ - an object
+and
+- a callback.
+ 
+ It should scan the object and return the first KEY for which the callback returns a truthy VALUE.
+ If no key is found, then it should return undefined.
+*/
+
+const findKey = function(object, callback) {
+
+  for (const key in object) {
+    if (callback(object[key]) === true) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2)); // => "noma"
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 4)); // => undefined
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ Assertion Passed: ${[actual]} === ${[expected]}`);
+  } else {
+    console.log(`🛑 🛑Assertion Failed: ${[actual]} !== ${[expected]}`);
+
+  }
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2), "noma");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 4), undefined);
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 3), "Akaleri");
---
+++ takeUntil.js
@@ -0,0 +1,71 @@
+/*
+It will take in two parameters :
+
+The array to work with
+The callback (which Lodash calls "predicate")
+
+The function will return a slice of the array with elements taken from the beginning. It should keep going until the callback/predicate returns a truthy value.
+To keep things simple, the callback should only be provided one value: The item in the array.
+
+Implement takeUntil which will keep collecting items from a provided array until the callback provided returns a truthy value.
+
+Expected Output
+[ 1, 2, 5, 7, 2 ]
+---
+[ 'I\'ve', 'been', 'to', 'Hollywood' ]
+
+*/
+
+const takeUntil = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    if (callback(item) === false)
+      results.push(item);
+    if (callback(item) === true) {
+      break;
+    }
+  }
+  return results;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅ Assertion Passed `);
+  } else {
+    console.log(`🛑 Assertion Failed `);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+
+  //check if arrays length match
+  //DOES NOT WORK ONLY IN assertArraysEqual.js file
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(assertArraysEqual(results1, [1, 2, 5, 7, 2]));
+
+console.log(assertArraysEqual(results2, ['I\'ve', 'been', 'to', 'Hollywood']));
+

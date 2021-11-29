0e3f4a4e956d99f21ef1f6221959c6156e551e0a
Olga <74125403+OlgaShilenkova@users.noreply.github.com>
2021-11-29 17:18:13 -0600

    Delete test directory

--- test/middleTest.js
+++
@@ -1,8 +0,0 @@
-const middle = require("../middle.js");
-
-const assertArraysEqual = require("../assertArraysEqual.js");
-
-
-
-assertArraysEqual(middle(["a", "b", "c", "d"]), ["b", "c"]);
-assertArraysEqual(middle(["a", "b", "c", "d"]), ["a", "c"]);
